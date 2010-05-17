e315d3cf0406c0fb4b698e231821a13dc3a7abc4
gasu.basu <gasu.basu@e5165e10-9dcc-11de-a8ff-c955b97a4a10>
2010-05-17 08:31:02 +0000

    remove junk

--- OpenGLCV/animation.h
+++
@@ -1,7 +0,0 @@
-void myKeyboardFunc( unsigned char key, int x, int y );
-void mySpecialKeyFunc( int key, int x, int y );
-
-void drawScene(void);
-
-void initRendering();
-void resizeWindow(int w, int h);
